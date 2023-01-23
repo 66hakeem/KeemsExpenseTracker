84ab00de918745156098ab2674b2a6d30b720c00
Syntax22 <syntax256@gmail.com>
2023-01-23 17:08:11 +0300

    Initial Commit

---
+++ KeemsExpenseTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KeemsExpenseTracker
+//
+//  Created by yinyang on 23/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KeemsExpenseTracker/KeemsExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeemsExpenseTrackerApp.swift
+//  KeemsExpenseTracker
+//
+//  Created by yinyang on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct KeemsExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
